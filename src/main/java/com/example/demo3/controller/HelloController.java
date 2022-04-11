287aa134dc1737b2da4f3771a73b80544df31459
hxtome <hxtome@163.com>
2022-04-12 00:33:55 +0800

    Initial commit

---
+++ src/main/java/com/example/demo3/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo3.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Autowired
+    StringRedisTemplate redisTemplate;
+    @RequestMapping(value = "hello")
+    public String hello(){
+        Long views =  redisTemplate.opsForValue().increment("views");
+        return "hello world,thanksyou views"+views;
+    }
+}
